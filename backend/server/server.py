153623a33c2d559a1e4a380112f8c77e21eca1b9
Huey Lee <hello@huey.xyz>
2024-03-31 22:01:24 +0800

    feature: init templates

--- backend/server/server.py
+++ backend/server/server.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_assets import Environment
+from flask_htmx import HTMX
 
 app = Flask(__name__)
+assets = Environment(app)
 htmx = HTMX(app)
 
 @app.route("/")
 def hello_world():
-  return "hi"
+  return render_template("index.html")
+
+if __name__ == "__main__":
+  assets.init_app(app)
